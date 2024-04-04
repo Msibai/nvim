1e7f726ea7fe19ea2346fe2558a8b05a67ba9f29
Mukhtar Sibai <mukhtar.sibai@gmail.com>
2024-04-04 11:36:38 +0200

    feat: Add tokyonight as colorscheme

--- lua/msibai/lazy.lua
+++ lua/msibai/lazy.lua
@@ -7,4 +7,6 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup({})
+require('lazy').setup({
+  {import = 'msibai.plugins'}
+})
