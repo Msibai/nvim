0e83266d0a3b83a35e0d7f4e2f8c240d3b690250
Mukhtar Sibai <mukhtar.sibai@gmail.com>
2024-04-07 16:04:17 +0200

    feat: Add toggleterm  plugin

---
+++ lua/msibai/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = '*',
+  opts = {
+    open_mapping = [[<C-`>]],
+    direction = 'float',
+    close_on_exit = true, -- close the terminal window when the process exits
+  },
+}
